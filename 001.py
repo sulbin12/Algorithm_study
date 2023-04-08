7b24e921428f0193ab4ce474194e67585776c044
limhyunbin <limgusqls@gmail.com>
2023-04-08 14:27:35 +0900

    day2

--- 001.py
+++ 001.py
@@ -1,4 +1,21 @@
-# 몫 구하기
-def solution(num1, num2):
-    answer = int(num1 / num2)
-    return answer
+list_a=[1,1,1,2,2,2,2,2,2,3,4,5]
+
+check_num_list=[0]*1000
+
+for data in list_a:
+    check_num_list[data]+=1
+
+max_idx=check_num_list.index(max(check_num_list))
+
+idx=0
+max_data_num=0
+for num in check_num_list:
+    if(max(check_num_list)==num):
+        max_data_num+=1
+
+if(max_data_num>1):
+    check=-1
+else:
+    check=max_idx
+
+print(check)
