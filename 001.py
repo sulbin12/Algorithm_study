d6a459103fbf5059923db13666bd8819e156837f
limgu <limgusqls@gmail.com>
2023-04-16 22:15:59 +0900

    day6

--- 001.py
+++ 001.py
@@ -1,21 +1,12 @@
-list_a=[1,1,1,2,2,2,2,2,2,3,4,5]
+my_string="hello"
+n=3
 
-check_num_list=[0]*1000
+list_string=list(my_string)
 
-for data in list_a:
-    check_num_list[data]+=1
-
-max_idx=check_num_list.index(max(check_num_list))
-
+answer=[]
 idx=0
-max_data_num=0
-for num in check_num_list:
-    if(max(check_num_list)==num):
-        max_data_num+=1
-
-if(max_data_num>1):
-    check=-1
-else:
-    check=max_idx
-
-print(check)
+for i in list_string:
+    answer+=(list_string[idx]*n)
+    idx+=1
+answer=''.join(answer)
+print(answer)
